111b70f18db1cafd9021241f7835f43116c6148f
welitoncardozo <welitoncardozo.br@gmail.com>
2021-05-06 18:05:46 -0300

    Created components to use in the all pages

--- sales-ui/src/App.tsx
+++ sales-ui/src/App.tsx
@@ -1,8 +1,18 @@
+import DataTable from "assets/components/DataTable";
+import Footer from "assets/components/Footer";
+import NavBar from "assets/components/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
+    <>
+      <NavBar/>
+      <div className="container">
+        <h1 className="text-primary">Hello world!</h1>
+
+        <DataTable/>
+      </div>
+      <Footer/>
+    </>
   );
 }
 
