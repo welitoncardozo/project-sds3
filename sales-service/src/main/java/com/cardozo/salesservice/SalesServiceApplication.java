4f29e8891f6a2f5505f93aaa814dec74ace3cdf1
welitoncardozo <welitoncardozo.br@gmail.com>
2021-05-05 22:46:48 -0300

    Initial commit

---
+++ sales-service/src/main/java/com/cardozo/salesservice/SalesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cardozo.salesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesServiceApplication.class, args);
+	}
+
+}
