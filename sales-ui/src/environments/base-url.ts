26e4e2ab2d3f6ea09670d94816580f51a1b91641
welitoncardozo <welitoncardozo.br@gmail.com>
2021-05-09 17:53:47 -0300

    Base URL.

--- sales-ui/src/environments/base-url.ts
+++ sales-ui/src/environments/base-url.ts
@@ -1,4 +1,4 @@
-const PRODUCTION = process.env.REACT_APP_BASE_URL_PRODUCTION;
-const DEVELOPMENT = 'http://localhost:8080';
+// const PRODUCTION = process.env.REACT_APP_BASE_URL_PRODUCTION;
+// const DEVELOPMENT = 'http://localhost:8080';
 
-export const BASE_URL = PRODUCTION ?? DEVELOPMENT;
+export const BASE_URL = process.env.REACT_APP_BASE_URL_PRODUCTION ?? 'http://localhost:8080';
